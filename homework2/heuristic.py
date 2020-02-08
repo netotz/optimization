a363efd1ed0cd651aec97d39d36d25f93ff9a836
netotz <neto.otz@hotmail.com>
2020-02-08 16:33:19 -0600

    Add heuristic function to solve a Knapsack problem

--- homework2/heuristic.py
+++ homework2/heuristic.py
@@ -1,2 +1,18 @@
 """Module for the heuristic technique to solve a Knapsack problem.
 """
+from knapsack import Knapsack
+
+def pickItems(knapsack: Knapsack, heuristic = 3):
+    '''Use the heuristic specified in the argument to get a solution for the knapsack problem.
+    '''
+    knapsack.sortItems(heuristic)
+    solution = list()
+    W = knapsack.capacity
+    for item in knapsack.items:
+        if item.weight <= W:
+            solution.append(item)
+            W -= item.weight
+            if W == 0:
+                break
+        elif heuristic == 2:
+            break
