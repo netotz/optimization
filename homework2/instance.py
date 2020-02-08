7d5171ec215889de1f3714d48026f7e5acb84c11
netotz <neto.otz@hotmail.com>
2020-02-08 01:51:31 -0600

    Refactor method to generate random Instance

--- homework2/instance.py
+++ homework2/instance.py
@@ -3,15 +3,16 @@
 from random import randint
 
 from item import Item
+from file_handling import getFilePath, generateFileName
 
 class Instance:
     '''Data of a basic Knapsack problem: number of items, knapsack's capacity and the items.
     
     Also includes methods to both generate and read an instance saved in a file.'''
-    def __init__(self, n, capacity, items):
+    def __init__(self, total_items, capacity, items):
         '''Constructs an Instance by specifying its data: n items, the capacity and a list of items.
         '''
-        self.total_items = n
+        self.total_items = total_items
         self.capacity = capacity
         self.items = items
 
@@ -19,9 +20,6 @@ class Instance:
     def generateRandom(cls, total_items, capacity, min_weight, max_weight, min_value, max_value):
         '''Constructs an random Instance.
         '''
-        items = list()
-        for index in range(total_items):
-            value = randint(min_value, max_value)
-            weight = randint(min_weight, max_weight)
-            items.append(Item(index, value, weight))
+        items = [Item(index, randint(min_value, max_value), randint(min_weight, max_weight)) for index in range(total_items)]
         return cls(total_items, capacity, items)
+
