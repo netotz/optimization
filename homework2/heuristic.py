b509240a558030e442cc47a0ed3eccd4ed2eaaad
netotz <neto.otz@hotmail.com>
2020-02-08 16:54:51 -0600

    Add Knapsack problem's objective function
    It sums the values of
    the items picked as solution

--- homework2/heuristic.py
+++ homework2/heuristic.py
@@ -1,6 +1,9 @@
 """Module for the heuristic technique to solve a Knapsack problem.
 """
+from typing import List
+
 from knapsack import Knapsack
+from item import Item
 
 def pickItems(knapsack: Knapsack, heuristic = 3):
     '''Use the heuristic specified in the argument to get a solution for the knapsack problem.
@@ -16,3 +19,12 @@ def pickItems(knapsack: Knapsack, heuristic = 3):
                 break
         elif heuristic == 2:
             break
+    return solution
+
+def sumValues(items: List[Item]):
+    '''Objective function of the Knapsack problem. Sums the value of each item in the items list.
+    '''
+    total_value = 0
+    for item in items:
+        total_value += item.value
+    return total_value
