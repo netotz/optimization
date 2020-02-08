1dfad400f0ce1d9f354854c94fc511c0e579eb8c
netotz <neto.otz@hotmail.com>
2020-02-08 00:32:00 -0600

    Add function to generate a file name to store an instance

--- homework2/file_handling.py
+++ homework2/file_handling.py
@@ -1,2 +1,11 @@
 """Module for reading and writing files that contain instances of a Knapsack problem.
 """
+from os.path import dirname, join
+
+def generateFileName(total_items, capacity, index = 0):
+    '''Generates a name for the file which will store the n items and capacity of an instance.
+
+    The index parameter distinguishes instances of same size.
+    '''
+    file_name = '_'.join([str(total_items), str(capacity), str(index)])
+    return file_name
