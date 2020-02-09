e3716641663b8e7411fbc73e5f48a5781eca3006
netotz <neto.otz@hotmail.com>
2020-02-08 18:27:09 -0600

    Convert Item's attributes into properties

--- homework2/item.py
+++ homework2/item.py
@@ -6,10 +6,27 @@ class Item:
     def __init__(self, index, value, weight):
         '''Construct an Item object by its value, its weight and an index to identify it.
         '''
-        self.index = index
-        self.value = value
-        self.weight = weight
-        self.ratio = value / weight
+        self.__index = index
+        self.__value = value
+        self.__weight = weight
+        self.__ratio = value / weight
+    
+    @property
+    def index(self):
+        return self.__index
+    
+    @property
+    def value(self):
+        return self.__value
+    
+    @property
+    def weight(self):
+        return self.__weight
+
+    @property
+    def ratio(self):
+        return self.__ratio
+
     def __str__(self):
         '''To String method.
         '''
