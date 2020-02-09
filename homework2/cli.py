0ef3c622dfd28df066d949a479dfd13083192ac7
netotz <neto.otz@hotmail.com>
2020-02-09 17:46:53 -0600

    Add prompt for generate n instances

--- homework2/cli.py
+++ homework2/cli.py
@@ -3,14 +3,38 @@
 from PyInquirer import prompt
 from examples import custom_style_1
 
+from validations import isPositiveInteger
+
+def inputDict(name, message):
+    '''Returns a dictionary of type input.
+    '''
+    return {
+        'type': 'input',
+        'name': name,
+        'message':  message,
+        'validate': lambda n: True if isPositiveInteger(n) else 'Please enter a valid positive integer.',
+        'filter': lambda n: int(n)
+    }
+
+def askForInstance():
+    '''Prompt for data to generate an instance.
+    '''
+    return ((
+        inputDict('n', 'How many items?'),
+        inputDict('p', 'What percentage of the items can fit in the knapsack?'),
+        inputDict('min v', 'How low can the value of an item be?'),
+        inputDict('max v', 'And how high?'),
+        inputDict('min w', 'How low can the weight of an item be?'),
+        inputDict('max w', 'And how high?')
+    ))
+
 def runCLI():
     '''Runs the options selector.
     '''
-    answers = dict()
-    which_menu = (
+    questions = (
         { # ask to generate or load an instance
             'type': 'list',
-            'name': 'option',
+            'name': 'menu',
             'message': 'What do you want to do?',
             'choices': (
                 {
@@ -25,8 +49,26 @@ def runCLI():
         }
     )
 
-    menu = prompt(which_menu, style=custom_style_1)
-    if menu == 1:
-        # TODO: generate
+    option = prompt(questions, style=custom_style_1)['menu']
+    if option == 1:
+        # generate
+        questions = (
+            {
+                'type': 'input',
+                'name': 'instances',
+                'message': 'How many instances?',
+                'validate': lambda n: True if isPositiveInteger(n) else 'Please enter a valid positive integer.',
+                'filter': lambda n: int(n)
+            }
+        )
+        instances = prompt(questions, style=custom_style_1)['instances']
+        for i in range(instances):
+            print('=== {}° instance ==='.format(i + 1))
+            questions = askForInstance()
+            answers = prompt(questions, style=custom_style_1)
+            
     else:
         # TODO: load
+        pass
+
+runCLI()
