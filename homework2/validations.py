9e34e56f3a0630406a383e31f8ace4a3831d7777
netotz <neto.otz@hotmail.com>
2020-02-09 18:11:01 -0600

    Add percentage validation

--- homework2/validations.py
+++ homework2/validations.py
@@ -1,6 +1,16 @@
 """Module for validating inputs.
 """
 
+def isValidPercentage(string, minimum, maximum):
+    '''If the string argument represents a valid percentage between the minimum and the maximum, returns True.
+    '''
+    if isPositiveNumber('float', string):
+        percentage = float(string)
+        if percentage >= minimum and percentage <= maximum:
+            return True
+    return False
+
+
 def isPositiveNumber(_type, string):
     '''If the string argument represents a valid positive type number, returns True.
     '''
