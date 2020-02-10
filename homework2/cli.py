ea940e74f123d8139c3f7c14eb72b1c3fabe093d
netotz <neto.otz@hotmail.com>
2020-02-09 19:49:37 -0600

    Add functions to validate inputs from CLI

--- homework2/cli.py
+++ homework2/cli.py
@@ -3,30 +3,55 @@
 from PyInquirer import prompt
 from examples import custom_style_1
 
-from validations import isPositiveInteger
+from validations import isPositiveNumber, isValidPercentage
+from knapsack import Knapsack
 
-def inputDict(name, message):
+# last given value
+_last_ = 0
+
+def saveLast(string):
+    '''Saves the last given value within the prompt.
+    '''
+    if isPositiveNumber('int', string):
+        global _last_
+        _last_ = int(string)
+        return True
+    return 'Please enter a valid positive integer.'
+
+def validateMax(string):
+    '''Returns True if the string represents a greater number than LAST, otherwise returns an error string.
+    '''
+    if isPositiveNumber('int', string):
+        integer = int(string)
+        global _last_
+        if integer > _last_:
+            return True
+        else:
+            return 'Please enter a number greater than the lower limit.'
+    return 'Please enter a valid positive integer.'
+
+def createInputQuestion(name, message, function = saveLast, cast = int):
     '''Returns a dictionary of type input.
     '''
     return {
         'type': 'input',
         'name': name,
         'message':  message,
-        'validate': lambda n: True if isPositiveInteger(n) else 'Please enter a valid positive integer.',
-        'filter': lambda n: int(n)
+        'validate': function,
+        'filter': lambda n: cast(n)
     }
 
-def askForInstance():
-    '''Prompt for data to generate an instance.
+def createInstanceQuestions():
+    '''Returns a tuple with questions asking for data to generate an instance.
     '''
-    return ((
-        inputDict('n', 'How many items?'),
-        inputDict('p', 'What percentage of the items can fit in the knapsack?'),
-        inputDict('min v', 'How low can the value of an item be?'),
-        inputDict('max v', 'And how high?'),
-        inputDict('min w', 'How low can the weight of an item be?'),
-        inputDict('max w', 'And how high?')
-    ))
+    return (
+        createInputQuestion('n', 'How many items?'),
+        createInputQuestion('p', 'What percentage of the items can fit in the knapsack?', isValidPercentage, float),
+        createInputQuestion('min v', 'How low can the value of an item be?'),
+        createInputQuestion('max v', 'And how high?', validateMax),
+        createInputQuestion('min w', 'How low can the weight of an item be?'),
+        createInputQuestion('max w', 'And how high?', validateMax)
+    )
 
 def runCLI():
     '''Runs the options selector.
@@ -52,23 +77,15 @@ def runCLI():
     option = prompt(questions, style=custom_style_1)['menu']
     if option == 1:
         # generate
-        questions = (
-            {
-                'type': 'input',
-                'name': 'instances',
-                'message': 'How many instances?',
-                'validate': lambda n: True if isPositiveInteger(n) else 'Please enter a valid positive integer.',
-                'filter': lambda n: int(n)
-            }
-        )
-        instances = prompt(questions, style=custom_style_1)['instances']
-        for i in range(instances):
-            print('=== {}° instance ==='.format(i + 1))
-            questions = askForInstance()
+        knapsacks = list()
+        i = 1
+        while True:
+            print('  === {}° instance ==='.format(i))
+            questions = createInstanceQuestions()
             answers = prompt(questions, style=custom_style_1)
-            
+            knapsacks.append(Knapsack.random(answers['n'], answers['min w'], answers['max w'], answers['min v'], answers['max v'], answers['p']))
+
+            i += 1
     else:
         # TODO: load
         pass
-
-runCLI()
