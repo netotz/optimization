5c42329e47bcf9ecb92540a4c64195ecbdf3873f
netotz <neto.otz@hotmail.com>
2020-02-09 20:59:20 -0600

    Add function to list existing files in current dir

--- homework2/file_handling.py
+++ homework2/file_handling.py
@@ -1,6 +1,7 @@
 """Module for reading and writing files that contain instances of a Knapsack problem.
 """
-from os.path import dirname, join
+from os import listdir
+from os.path import dirname, join, isfile
 
 def generateFileName(total_items, capacity, index = 0):
     '''Generates a name for the file which will store the n items and capacity of an instance.
@@ -12,6 +13,13 @@ def generateFileName(total_items, capacity, index = 0):
 def getFilePath(file_name):
     '''Returns the path of file_name.
     '''
-    subdirectory = dirname(__file__)
-    file_path = join(subdirectory, 'instances/{}'.format(file_name))
+    current_directory = dirname(__file__)
+    file_path = join(current_directory, 'instances/{}'.format(file_name))
     return file_path
+
+def listFiles():
+    '''Returns a list with the .dat files in the instances/ subdirectory.
+    '''
+    current_directory = dirname(__file__)
+    subdirectory = join(current_directory, 'instances\\')
+    return [file for file in listdir(subdirectory) if isfile(join(subdirectory, file)) and file[-3:].lower() == 'dat']
